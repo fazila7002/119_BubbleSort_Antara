1f3bb55b399f76ddfb7c0efc01bcf5e2cd520484
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-14 15:28:40 +0700

    looping untuk displaynya karena akan di tampilkan secara berurutan

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -37,5 +37,9 @@ void display() {
     cout << "========================="<< endl;
     cout << "Elemen Array telah diurut"<< endl;
     cout << "========================="<< endl;
-    
+
+    for (int i = 0 ; i < n ; i++)
+    {
+        cout<< "Data ke-" << i << " : " << a[i] << endl;
+    }
 }
