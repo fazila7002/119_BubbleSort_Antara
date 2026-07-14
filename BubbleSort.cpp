9d5d0a2ef0ae23e0efc68f69bb0c451819e8dacf
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-14 15:22:58 +0700

    ini adalaha looping for, dan looping akan dimulai dari 0, kenapa looping? karena ini akan terjadi perulangan saat menginput elemen sejumlah n

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -24,4 +24,10 @@ void input(){
     cout << "Masukkan elemen array" << endl;
     cout << "======================"<< endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Masukkan Nilai ke - " << i+1 << " : " ;
+        cin >> a[i];
+    }
+
 }
